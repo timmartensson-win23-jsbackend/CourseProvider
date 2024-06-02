c15ebddb181c88decba60416ed3f7d5176e1ad0d
AnnSofie <annsofie.sjolander@gmail.com>
2024-06-03 00:03:50 +0200

    minor changes

--- Infrastructure/GraphQL/Mutations/CourseMutation.cs
+++ Infrastructure/GraphQL/Mutations/CourseMutation.cs
@@ -16,17 +16,7 @@ public class CourseMutation(ICourseService courseService)
         return await _courseService.CreateCourseAsync(input);
     }
 
-    //[GraphQLName("createCourse")]
-    //public async Task<CreateCourseResponse> CreateCourseAsync(CourseCreateRequest input)
-    //{
-    //    var course = await _courseService.CreateCourseAsync(input);
-    //    return new CreateCourseResponse
-    //    {
-    //        Id = course.Id,
-    //        Success = true,
-    //        Message = "Course created successfully"
-    //    };
-    //}
+   
 
     [GraphQLName("updateCourse")]
     public async Task<Course> UpdateCourseAsync(CourseUpdateRequest input)
